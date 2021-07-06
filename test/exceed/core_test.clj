1162ed49c30896b6923b40ebb1c7092a4623de10
Jungy <jungorend@gmail.com>
2021-07-06 00:51:41 -0400

    Add boost tests

--- test/exceed/core_test.clj
+++ test/exceed/core_test.clj
@@ -2,14 +2,22 @@
   (:require [clojure.test :refer :all]
             [exceed.core :refer :all]))
 
+(defn confirm-space
+  "Helper function to return a specific space's objects"
+  [game space]
+  ((:play-area game) space))
+
 (deftest basic-initialization
   (let [game (setup-game :ryu :ken)]
     (testing "Confirm that Ryu and Ken are in starting positions"
-      (is (= [[:p1 :ryu]] ((:play-area game) 2)))
-      (is (= [[:p2 :ken]] ((:play-area game) 6))))))
+      (is (= [[:p1 :ryu]] (confirm-space game 2)))
+      (is (= [[:p2 :ken]] (confirm-space game 6))))))
 
 (deftest boosts
-  (let [game (setup-game :ryu :ken)]
+  (let [game (setup-game :ryu :ken)
+        cant-move-set (assoc-in game [:p1 :status :can-move] false)]
     (testing "Confirm that Retreat moves back 2 spaces"
-      (is (= [[:p1 :ryu]] ((:play-area
-                             ((get-in normals [:assault :boost-text]) nil game :p1)) 0))))))
+      (is (= [[:p1 :ryu]] (confirm-space ((get-in normals [:assault :boost-text]) nil game :p1) 0)))
+      (is (= [[:p2 :ken]] (confirm-space ((get-in normals [:assault :boost-text]) nil game :p2) 8))))
+    (testing "Doesn't Retreat when can't move is set"
+      (is (= [[:p1 :ryu]] (confirm-space ((get-in normals [:assault :boost-text]) nil cant-move-set :p1) 2))))))
