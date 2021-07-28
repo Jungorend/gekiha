db9a41e73976d8ead791334646ce893672b7b6ee
Jungy <jungorend@gmail.com>
2021-07-28 00:01:55 -0400

    Move sample text to js for easier testing of templating.

--- src/cljs/exceed/exceed.core.cljs
+++ src/cljs/exceed/exceed.core.cljs
@@ -1,13 +1,23 @@
 (ns exceed.core
-    (:require [reagent.core :as r]
-              [reagent.dom :as dom]))
+  (:require [reagent.core :as r]
+            [reagent.dom :as dom]))
 
-(def game-state (r/atom ["<h1>Is state working?</h1>"]))
+(def game-state (r/atom [[:p "Player boosted Backstep."]
+                         [:p "Opponent initiated a strike."]
+                          [:hr] [:h3 {:class "strike"} "Strike!"]
+                          [:p "Player had Cross."]
+                          [:p "Opponent had EX-Block."]
+                          [:p "Opponent spent 1 force on Block."]
+                         [:hr] [:h3 {:class "turn"} "Turn 4"]
+                          [:p "Player spent a Grasp from hand to move 1."]
+                         [:hr] [:h3 {:class "turn"} "Turn 7"]]))
 
 (defn add-history! [new-state]
-    (swap! game-state (conj @game-state new-state)))
+  (swap! game-state (conj @game-state new-state)))
 
 (defn display-history []
-    (fn []
-        [:div.history>div#history-text
-         (first @game-state)]))
+  (fn []
+    (apply vector :ul @game-state)))
+
+(dom/render
+  [display-history] (.getElementById js/document "history-text"))
