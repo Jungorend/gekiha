4a02067cfe9a08112dba6438fb5c398a50c5dfca
Jungy <jungorend@gmail.com>
2021-08-20 04:50:46 -0400

    Re-frame now handles game history.

--- src/cljs/exceed/core.cljs
+++ src/cljs/exceed/core.cljs
@@ -4,20 +4,29 @@
             [re-frame.core :as rf]
             [ajax.core :refer [GET POST]]))
 
-(def game-state (r/atom {}))
+(rf/reg-event-db
+  :init
+  (fn [_ _]
+    {:game {}}))
 
-(defn add-history! [new-state]
-  (swap! game-state (conj @game-state new-state)))
+(rf/reg-event-db
+  :set-history
+  (fn [db [_ history]]
+    (assoc db :game history)))
 
-(defn display-history []
-  (fn []
-    (apply vector :ul (:game-history @game-state))))
+(rf/reg-sub
+  :history
+  (fn [db _]
+    (get-in db [:game :game-history])))
+
+(defn view-history
+  []
+  (apply vector :ul @(rf/subscribe [:history])))
 
 (defn get-gamestate []
   (GET "/game-state"
        {:headers       {"Accept" "application/transit+json"}
-        :handler       #((do (reset! game-state %)
-                             (dom/render [display-history] (.getElementById js/document "history-text"))))
+        :handler       #(rf/dispatch [:set-history %])
         :error-handler #(.log js/console (str "Error: " %))}))
 
 (def game-outline
@@ -31,13 +40,16 @@
       [:li [:a {:href "/account"} "My Account"]]]]
     [:div.history
      [:img#scroll-history-up.scroll-btn {:src "/img/arrow_circle_up_black_24dp.svg" :alt "scroll history up"}]
-     [:div#history-text]
+     [:div#history-text
+      [view-history]]
      [:img#scroll-history-down.scroll-btn {:src "/img/arrow_circle_down_black_24dp.svg" :alt "scroll history down"}]]]
    [:div.game-container
     [:canvas#game]]
    [:div.footer>p "Hypothetical cards go here."]
    [:script {:type "text/javascript" :src "/js/app.js"}]])
 
-(dom/render game-outline (.-body js/document))
+(rf/dispatch-sync [:init])
+
+(dom/render game-outline (.getElementById js/document "main-container"))
 
 (get-gamestate)
